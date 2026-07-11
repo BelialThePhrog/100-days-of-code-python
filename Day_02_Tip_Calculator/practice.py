b034d4633260b8b0a698bf0fb095f2fbef2ec903
Kacper Cichy <kacperdok@gmail.com>
2026-07-11 17:37:51 +0200

    Create practice.py

---
+++ Day_02_Tip_Calculator/practice.py
@@ -0,0 +1,9 @@
+# Day 2 Learning - Length function and String manipulation
+
+# Using len() function
+print(len("Hello"))
+print(len(str(12313414)))
+
+# Selecting letters from strings
+print("Hello"[0])
+print("Hello"[-1])
