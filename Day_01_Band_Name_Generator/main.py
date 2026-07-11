b4cdc70832e54b9045caec8e5bdb4cabecef25a2
Kacper Cichy <kacperdok@gmail.com>
2026-07-11 17:36:41 +0200

    Create main.py

---
+++ Day_01_Band_Name_Generator/main.py
@@ -0,0 +1,6 @@
+# Day 1 Project - Band Name Generator
+
+print("Welcome to the Band Name Generator!")
+city = input("What's the City you grew up in? ")
+pet_name = input("What's your pet's name? ")
+print("Your Band Name: " + city + " " + pet_name + "!")
