701a7ee74453aa11ef040efca9d77ff90e31c236
Kacper Cichy <kacperdok@gmail.com>
2026-07-11 17:34:16 +0200

    Enhance practice.py with print and input examples
    Added examples of print functions, string concatenation, and user input.

--- Day_01_Band_Name_Generator/practice.py
+++ Day_01_Band_Name_Generator/practice.py
@@ -1,1 +1,18 @@
+# Day 1 Learning - Print func, variables, etc.
 
+print("Hello World!")
+
+# Making it to the new line
+print("Hello \nWorld")
+
+# Combining different strings
+print("Hello World" + " Stacy")
+a = "Hello World"
+b = " Stacy"
+print(a + b)
+
+# Input function
+a = input("What's your name? ")
+b = len(a)
+print("Hello " + a + "!")
+print("Your name has " + str(b) + " characters")
