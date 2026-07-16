deea170c6e58aece91ace9ddbbf0e8ec2c6a4330
Kacper Cichy <kacperdok@gmail.com>
2026-07-16 09:51:38 +0200

    Create practice.py

---
+++ Day_08_Ceasar_Cipher/practice.py
@@ -0,0 +1,14 @@
+# Day 8 Learning - Functions with Inputs
+
+def greet(name):
+    print("Hello")
+    print(f"How are you {name}?")
+    print("Let's do this!")
+
+# Calling with positional argument
+greet("Kacper")
+
+print("-" * 20)
+
+# Calling with keyword argument
+greet(name="Kacper")
