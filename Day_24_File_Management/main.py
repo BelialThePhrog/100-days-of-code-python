4afd600260c57b58f510288a47de7d2e0a267926
Kacper Cichy <kacperdok@gmail.com>
2026-07-30 09:18:42 +0200

    Implement basic Snake game with collision detection

---
+++ Day_24_File_Management/main.py
@@ -0,0 +1,47 @@
+import turtle as tur
+import time
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+
+screen = tur.Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.2)
+    snake.move()
+
+    # Detect collision with food
+    if snake.segments[0].distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
+        snake.extend()
+        
+    # Detect collision with wall
+    if snake.segments[0].xcor() > 280 or snake.segments[0].xcor() < -280 or snake.segments[0].ycor() > 280 or snake.segments[0].ycor() < -280:
+        scoreboard.reset()
+        snake.reset()
+
+    # Detect collision with tail
+    for seg in snake.segments[1:]:
+        if snake.segments[0].distance(seg) < 10:
+            scoreboard.reset()
+            snake.reset()
+    
+screen.exitonclick()
