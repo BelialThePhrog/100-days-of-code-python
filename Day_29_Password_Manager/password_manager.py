43cb95d1bb84256f0626286a5309495ebb0b689e
Kacper Cichy <kacperdok@gmail.com>
2026-08-06 10:15:59 +0200

    Implement password manager with GUI and features

---
+++ Day_29_Password_Manager/password_manager.py
@@ -0,0 +1,100 @@
+import tkinter as tk
+from tkinter import messagebox
+import time
+import math
+FONT_NAME = "Courier"
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+import random as r
+
+letters = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+numbers = list("0123456789")
+
+symbols = [
+    "!", "@", "#", "$", "%", "^", "&", "*",
+    "(", ")", "-", "_", "=", "+",
+    "[", "]", "{", "}", "\\", "|",
+    ";", ":", "'", '"', ",", ".",
+    "<", ">", "/", "?", "`", "~"
+]
+
+
+def pass_generate():
+    password = ""
+    for letter in range(0,20):
+        l = r.randint(0,51)
+        password += letters[l]
+    for number in range(0,5):
+        n = r.randint(0,9)
+        password += numbers[n]
+    for symbol in range(0,5):
+        s = r.randint(0,31)
+        password += symbols[s]
+    new_password = list(password)
+    r.shuffle(new_password)
+    new_final_password = "".join(new_password)
+    password_entry.insert(0, new_final_password)
+    window.clipboard_clear()
+    window.clipboard_append(new_final_password)
+    
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def add_password():
+    value_1 = website_entry.get()
+    value_2 = email_entry.get()
+    value_3 = password_entry.get()
+    with open("data.txt", mode="a") as file:
+        file.write(f" {value_1} | {value_2} | {value_3} \n")
+    messagebox.showerror("Saved!", "Your password has been saved!")
+    website_entry.delete(0, "end")
+    password_entry.delete(0, "end")
+    email_entry.delete(0, "end")
+    email_entry.insert(0, "your_email@gmail.com")
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = tk.Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+# --- LOGO (Row 0) ---
+
+canvas = tk.Canvas(height=200, width=200)
+
+logo_img = tk.PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0, column=1)
+
+# --- LABELS (Column 0) ---
+website_label = tk.Label(text="Website:")
+website_label.grid(row=1, column=0)
+
+email_label = tk.Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+
+password_label = tk.Label(text="Password:")
+password_label.grid(row=3, column=0)
+
+# --- ENTRIES & BUTTONS ---
+
+# Website Entry: Spans across Column 1 and Column 2
+website_entry = tk.Entry()
+website_entry.grid(row=1, column=1, columnspan=2, sticky="EW")
+website_entry.focus() 
+
+# Email Entry: Spans across Column 1 and Column 2
+email_entry = tk.Entry()
+email_entry.grid(row=2, column=1, columnspan=2, sticky="EW")
+email_entry.insert(0, "your_email@gmail.com") 
+
+# Password Entry: Sits only in Column 1
+password_entry = tk.Entry()
+password_entry.grid(row=3, column=1, sticky="EW")
+
+# Generate Password Button: Sits only in Column 2
+generate_password_button = tk.Button(text="Generate Password", command=pass_generate)
+generate_password_button.grid(row=3, column=2, sticky="EW")
+
+# Add Button: Spans across Column 1 and Column 2
+add_button = tk.Button(text="Add", command=add_password)
+add_button.grid(row=4, column=1, columnspan=2, sticky="EW")
+
+window.mainloop()
